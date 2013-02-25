2c36d54b3e578584b96ebf331eaf50b316a2c87f
Dejan Simic <desimic@gmail.com>
2013-02-25 20:12:02 +0100

    typo

--- lib/srbovanje/string_ext.rb
+++ lib/srbovanje/string_ext.rb
@@ -4,7 +4,7 @@ String.class_eval do
 
   # Preslovljava na latinicu
   # Params:
-  # +charset+:: ako je :ascii preslovljava u "ošišanju" latinicu.
+  # +charset+:: ako je :ascii preslovljava u "ošišanu" latinicu.
   def na_latinici(charset=:utf8)
     mapping = Srbovanje.latinica_map(charset)
     self.gsub(/.+?/) do |chr|
