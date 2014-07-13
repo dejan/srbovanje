460dd7765b348bb5f3d470735d836e2c881d00bc
Dejan Simic <desimic@gmail.com>
2014-07-13 06:11:22 +0200

    utisaj warning "[deprecated] I18n.enforce_available_locales will ..."

--- lib/srbovanje/railtie.rb
+++ lib/srbovanje/railtie.rb
@@ -1,5 +1,6 @@
 module Srbovanje 
   class Railtie < Rails::Railtie
+    config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :sr
     config.after_initialize do
       require 'i18n_config'
