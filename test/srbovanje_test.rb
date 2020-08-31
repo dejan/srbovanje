5f38f6adae377b5f2393175e6f063e187962e47d
dejan <desimic@gmail.com>
2020-08-31 06:37:41 +0200

    Preferiraj ' umesto "

--- test/srbovanje_test.rb
+++ test/srbovanje_test.rb
@@ -5,14 +5,14 @@ require 'test_helper'
 describe Srbovanje do
   it 'preslovljava cirilicu u latinicu' do
     examples = [
-      ["ЊЕГОШ", "NJEGOŠ", "NJEGOS"],
-      ["Latinica", "Latinica", "Latinica"],
-      ["Ђурђевак", "Đurđevak", "Djurdjevak"],
-      ["Шабан Шаулић", "Šaban Šaulić", "Saban Saulic"],
-      ["електродистрибуција", "elektrodistribucija", "elektrodistribucija"],
-      ["ЂОРЂЕ Ђ. Ђорђевић", "ĐORĐE Đ. Đorđević", "DJORDJE Dj. Djordjevic"],
-      ["ĐORĐE Đ. Đorđević", "ĐORĐE Đ. Đorđević", "DJORDJE Dj. Djordjevic"],
-      ["ЉИЉА Љ. Љиљановић", "LJILJA Lj. Ljiljanović", "LJILJA Lj. Ljiljanovic"]
+      ['ЊЕГОШ', 'NJEGOŠ', 'NJEGOS'],
+      ['Latinica', 'Latinica', 'Latinica'],
+      ['Ђурђевак', 'Đurđevak', 'Djurdjevak'],
+      ['Шабан Шаулић', 'Šaban Šaulić', 'Saban Saulic'],
+      ['електродистрибуција', 'elektrodistribucija', 'elektrodistribucija'],
+      ['ЂОРЂЕ Ђ. Ђорђевић', 'ĐORĐE Đ. Đorđević', 'DJORDJE Dj. Djordjevic'],
+      ['ĐORĐE Đ. Đorđević', 'ĐORĐE Đ. Đorđević', 'DJORDJE Dj. Djordjevic'],
+      ['ЉИЉА Љ. Љиљановић', 'LJILJA Lj. Ljiljanović', 'LJILJA Lj. Ljiljanovic']
     ]
 
     examples.each do |input, latin, ascii|
